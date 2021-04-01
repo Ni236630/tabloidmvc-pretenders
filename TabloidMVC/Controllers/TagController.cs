7828361bd35e76340d1c40ea95372e82aae36300
Rhowie86 <ronaldnhowie@gmail.com>
2021-04-01 14:44:52 -0500

    Tag Create and Edit

---
+++ TabloidMVC/Controllers/TagController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class TagController : Controller
+    {
+
+        private readonly ITagRepository _tagRepository;
+        private readonly IPostRepository _postRepository;
+
+        public TagController(ITagRepository tagRepository, IPostRepository postRepository)
+        {
+            _tagRepository = tagRepository;
+            _postRepository = postRepository;
+        }
+
+
+
+
+        // GET: TagController
+        public ActionResult Index()
+        {
+            List<Tag> tags = _tagRepository.GetAll();
+
+            return View(tags);
+        }
+
+        // GET: TagController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TagController/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: TagController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Tag tag)
+        {
+            try
+            {
+                _tagRepository.AddTag(tag);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: TagController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Tag tag = _tagRepository.GetTagById(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+
+        // POST: TagController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Tag tag)
+        {
+            try
+            {
+                _tagRepository.EditTag(tag);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TagController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TagController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public interface ITagRepository
+    {
+        List<Tag> GetAll();
+
+        Tag GetTagById(int id);
+
+        void AddTag(Tag tag);
+
+        void EditTag(Tag tag);
+    }
+}
